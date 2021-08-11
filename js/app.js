6b2b376530a2840d6a7382c51c5ba98c97137003
itsjustrobert <robertlove99999@gmail.com>
2021-08-11 17:23:49 -0500

    lab 13

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 //  here is where we will define classes/instantiate clickable
+// all instances will be stored in this array
 
+const allPictures = [];
 // get a random picture
-var mixer = () => {
-  return Math.floor(Math.random()) * imagesArray.length;
+function mixer() {
+  return Math.floor(Math.random() * imagesArray);
 };
 
 // define base set of properties for our object
@@ -15,6 +17,8 @@ class Pictures {
   constructor(name, imgSrc) {
     this.name = name;
     this.imgSrc = imgSrc;
+    allPictures.push(this);
+ 
   }
 }
 
@@ -23,7 +27,7 @@ let pageLeft = null;
 let pageMiddle = null;
 let pageRight = null;
 // rules
-const totalClicks = 0;
+let totalClicks = 0;
 const maxClicksAllowed = 5;
 
 // randomly pull from a list of image objects
@@ -41,7 +45,7 @@ let imagesArray = [
   new Pictures("helloo", "./assets/images/food.jpg"),
 ];
 console.log(imagesArray);
-
+console.log(allPictures);
 // set up our elements refrences in the dom
 let header = document.getElementById("header");
 let section = document.getElementById("allPics");
@@ -56,7 +60,7 @@ let start = document.getElementById("startBtn");
 let startText = document.createTextNode("Start");
 start.appendChild(startText);
 // =================================================================
-// implement a function to pic 2 random pictures
+// implement a function to pic 3 random pictures
 function newPick() {
   let rightIndex = Math.floor(Math.random() * imagesArray.length);
 
@@ -78,19 +82,40 @@ function newPick() {
   pageMiddle = imagesArray[middleIndex];
   centerImageText = imagesArray[middleIndex].name;
   centerImage.src = imagesArray[middleIndex].imgSrc;
+  // change pictues
+  const changePictues = () => {
+    rightImage.addEventListener("click", newPick);
+    leftImage.addEventListener("click", newPick);
+    centerImage.addEventListener("click", newPick);
+
+    if (
+      imagesArray[rightIndex] === imagesArray[leftIndex] ||
+      imagesArray[middleIndex] === imagesArray[rightIndex] ||
+      imagesArray[middleIndex] === imagesArray[leftIndex]
+    ) {
+      // logic is correct  
+      console.log("you have a dupe");
+      // imagesArray[rightIndex] = mixer();
+      // imagesArray[middleIndex] = mixer();
+      // imagesArray[leftIndex] = mixer();
+
+    }
+  };
+  changePictues();
+
 
   //   check for duplicates
 
-if(
-    pageLeft === pageMiddle ||
-    pageLeft === pageRight ||
-    pageMiddle === pageRight
-  ) {
-    pageRight = mixer(imagesArray);
-    pageLeft = mixer(imagesArray);
-    pageMiddle = mixer(imagesArray);
-    // console.log('hello world')
-  }
+  // if(
+  //     pageLeft === pageMiddle ||
+  //     pageLeft === pageRight ||
+  //     pageMiddle === pageRight
+  //   ) {
+  //     pageRight = mixer(imagesArray);
+  //     pageLeft = mixer(imagesArray);
+  //     pageMiddle = mixer(imagesArray);
+  //     // console.log('hello world')
+  //   }
 
   // keep up with the two randomly picked objects
 
@@ -102,6 +127,141 @@ function clickOnProduct(evt) {
   const thingsClicked = evt.target;
   const id = thingsClicked.id;
   // check to see which event happened
+}
+function renderChart() {
+  const ctx = document.getElementById("canvas").getContext("2d");
+
+  const productVotes = [];
+  const productTitles = [];
+  const productViews = [];
+
+  for (let index = 0; index < allPictures.length; index++) {
+    const product = allPictures[index];
+  }
+
+  chart = new Chart(ctx, {
+    type: "horizontalBar",
+    data: {
+      labels: productTitles,
+      datasets: [
+        {
+          label: "# of Votes",
+          data: productVotes,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.7)",
+            "rgba(54, 162, 235, 0.7)",
+            "rgba(255, 206, 86, 0.7)",
+            "rgba(75, 192, 192, 0.7)",
+            "rgba(153, 102, 255, 0.7)",
+            "rgba(255, 159, 64, 0.7)",
+            "rgba(255, 99, 132, 0.7)",
+            "rgba(54, 162, 235, 0.7)",
+            "rgba(255, 206, 86, 0.7)",
+            "rgba(75, 192, 192, 0.7)",
+            "rgba(153, 102, 255, 0.7)",
+            "rgba(255, 159, 64, 0.7)",
+            "rgba(255, 99, 132, 0.7)",
+            "rgba(54, 162, 235, 0.7)",
+            "rgba(255, 206, 86, 0.7)",
+            "rgba(75, 192, 192, 0.7)",
+            "rgba(153, 102, 255, 0.7)",
+            "rgba(255, 159, 64, 0.7)",
+            "rgba(255, 99, 132, 0.7)",
+            "rgba(54, 162, 235, 0.7)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+          ],
+          borderWidth: 1,
+        },
+        {
+          label: "# of views",
+          data: productViews,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              stepSize: 1,
+            },
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+      },
+    },
+  });
 }
 
 // poe
